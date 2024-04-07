ae84bf286998243c266cf80fbc86e8a0ff6751ec
EmilioColds <emiliofriastapia2705@outlook.com>
2024-04-07 17:04:01 -0600

    Finished initial server.js setup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./config/connection');
 const Sequelize = sequelize.Sequelize;
+const { homeRoutes, dashboardRoutes } = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,3 +20,18 @@ const sess = {
 };
 
 app.use(session(sess));
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Routes will be declared here
+app.use('/', homeRoutes);
+app.use('/', dashboardRoutes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
+});

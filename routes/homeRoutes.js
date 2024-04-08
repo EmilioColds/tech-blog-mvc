badb4ebf26948f1ac7fd038d97c861fbc0e852e6
EmilioColds <emiliofriastapia2705@outlook.com>
2024-04-08 17:19:31 -0600

    Fixed some typos

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -54,7 +54,7 @@ router.post('/login', async (req, res) => {
         }
 
         req.session.save(() => {
-            req.session.userId = userData.isSoftDeleted;
+            req.session.userId = userData.id;
             req.session.username = userData.username;
             req.session.loggedIn = true;
 

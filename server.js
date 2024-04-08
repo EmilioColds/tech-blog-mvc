37238bdb15f43207ed4c318c06fbd30fefde880f
EmilioColds <emiliofriastapia2705@outlook.com>
2024-04-08 14:21:48 -0600

    Finished the index.js for routes and updated the server.js

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./config/connection');
-const Sequelize = sequelize.Sequelize;
-const { homeRoutes, dashboardRoutes } = require('./routes');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
     secret: process.env.SESSION_SECRET,
-    cookie: {},
+    cookie: { maxAge: 15 * 60 * 1000 }, //Session will expire after 15 minutes
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
@@ -27,10 +26,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
-// Routes will be declared here
-app.use('/', homeRoutes);
-app.use('/', dashboardRoutes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));

cd2fb423396451003bdeb15e7b5cb0586e4d1fc2
EmilioColds <emiliofriastapia2705@outlook.com>
2024-04-09 13:52:29 -0600

    Fixed the DOM content loader in login

--- public/js/login.js
+++ public/js/login.js
@@ -1,26 +1,31 @@
-async function loginHandler(event) {
-    event.preventDefault();
-
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-
-    if(username && password) {
-        const response = await fetch('/login', {
-            method: 'POST',
-            body: JSON.stringify({
-                username,
-                password,
-            }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-
-        if(response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            const data = await response.json();
-            alert(data.message);
+document.addEventListener('DOMContentLoaded', () => {
+    async function loginHandler(event) {
+        event.preventDefault();
+    
+        const username = document.querySelector('#username-login').value.trim();
+        const password = document.querySelector('#password-login').value.trim();
+    
+        if(username && password) {
+            const response = await fetch('/login', {
+                method: 'POST',
+                body: JSON.stringify({
+                    username,
+                    password,
+                }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+    
+            if(response.ok) {
+                document.location.replace('/dashboard');
+            } else {
+                const data = await response.json();
+                alert(data.message);
+            }
         }
     }
-}
 
-document.querySelector('.login-form').addEventListener('submit', loginHandler);
+    const loginForm = document.querySelector('.login-form');
+    if (loginForm) {
+        loginForm.addEventListener('submit', loginHandler);
+    }
+});

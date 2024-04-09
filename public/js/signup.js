425e885df24db63fcb57ea0a5ba8e9edfda9ab73
EmilioColds <emiliofriastapia2705@outlook.com>
2024-04-09 13:54:25 -0600

    Added the DOM content loader a signup file

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,25 +1,30 @@
-async function signupHandler(event) {
-    event.preventDefault();
-
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-
-    if(username && password) {
-        const response = await fetch('/signup', {
-            method: 'POST',
-            body: JSON.stringify({
-                username,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-
-        if(response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert('Not logged in!');
+document.addEventListener('DOMContentLoaded', () => {
+    async function signupHandler(event) {
+        event.preventDefault();
+    
+        const username = document.querySelector('#username-signup').value.trim();
+        const password = document.querySelector('#password-signup').value.trim();
+    
+        if(username && password) {
+            const response = await fetch('/signup', {
+                method: 'POST',
+                body: JSON.stringify({
+                    username,
+                    password
+                }),
+                headers: { 'Content-Type': 'application/json' }
+            });
+    
+            if(response.ok) {
+                document.location.replace('/dashboard');
+            } else {
+                alert('Not logged in!');
+            }
         }
     }
-}
 
-document.querySelector('.signup-form').addEventListener('submit', signupHandler);
+    const signupForm = document.querySelector('.signup-form');
+    if(signupForm) {
+        signupForm.addEventListener('submit', signupHandler);
+    }
+});

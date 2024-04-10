69a6ecce033a5fca6cb76a000633813f975de1e8
EmilioColds <emiliofriastapia2705@outlook.com>
2024-04-10 12:34:37 -0600

    Fixed the invalid date issue

--- public/js/logout.js
+++ public/js/logout.js
@@ -19,3 +19,22 @@ document.addEventListener("DOMContentLoaded", () => {
     logout();
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const logoutLink = document.querySelector("#logout");
+    if (logoutLink) {
+        logoutLink.addEventListener("click", async (e) => {
+            e.preventDefault();
+            const response = await fetch("/logout", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+            });
+
+            if (response.ok) {
+                document.location.replace('/homepage');
+            } else {
+                alert("----- Failed to log out. -----");
+            }
+        });
+    }
+});
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,7 @@
 const moment = require('moment');
 
 module.exports = {
-    formatDate: (date, format = 'YYYY-MM-DD') => {
-        if (typeof date !== 'string' || !moment(date).isValid()) {
-            console.error(`Invalid date: ${date}`);
-            return 'Invalid date';
-        }
-        return moment(date).format(format);
+    formatDate: (date, format = 'YYYY-MM-DD HH:mm:ss') => {
+        return moment(date).isValid() ? moment(date).format(format) : 'Invalid date';
     }
-}
+};
